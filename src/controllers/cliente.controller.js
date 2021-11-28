f2c438d1f6b68b6c286314389f5864145f1ad025
andylopezgit <alopezciotta@gmail.com>
2021-11-28 09:16:55 -0300

    agrego controladores de clientes

--- src/controllers/cliente.controller.js
+++ src/controllers/cliente.controller.js
@@ -1,15 +1,99 @@
 import Cliente from "..//models/cliente.model";
 
 let ClienteController = {
-  getClientes() {},
+  getClientes: async (res) => {
+    try {
+      const clientes = Cliente.find();
+      res
+        .status(200)
+        .json(cliente);
+    } catch (error) {
+      res
+      console.log(error)
 
-  saveCliente() {},
+    }
+  },
 
-  getCliente() {},
+  saveCliente: async (req, res) => {
+    const body = req.body
+    console.log(body)
+    try {
+      const savedCliente = await Cliente.create(body);
+      res
+        .status(201)
+        .json(savedCliente);
+    } catch (error) {
+      res
+        .status(500)
+        .json({
+          mensage: error
+        })
 
-  deleteCliente() {},
+    }
+  },
 
-  updatedCliente() {},
+  getCliente: async (req, res) => {
+    const _id = req.params.id;
+    try {
+      const cliente = await Cliente.findOne({ _id });
+      res
+        .status(200)
+        .json(cliente)
+    } catch (error) {
+      res
+        .status(400)
+        .json({
+          message: err
+        })
+    }
+  },
+
+  deleteCliente: async (req, res) => {
+    const _id = req.params.id;
+    try {
+      const revomedcliente = await cliente.findByIdAndDelete({_id});
+
+      if (!revomedcliente) {
+        return res.status(404).json({
+          message: err
+        })
+      }
+
+      res.json(revomedcliente)
+    } catch (err) {
+      res
+        .status(400)
+        .json({
+          message: err
+        })
+    }
+  },
+
+  updatedCliente: async (req, res) => {
+    const _id = req.params.id;
+    const body = req.body;
+    try {
+      const updatedCliente = await Cliente.findByIdAndUpdate(
+        _id,
+        body,
+        {new: true});
+
+      if (!updatedCliente) {
+        return res.status(404).json({
+          message: err
+        })
+      }
+      res
+        .status(200)
+        .json(updatedCliente)
+    } catch (err) {
+      res
+        .status(500)
+        .json({
+          message: err
+        })
+    }
+  }
 };
 
 export default ClienteController;
--- src/models/retiro.model.js
+++ src/models/retiro.model.js
@@ -2,12 +2,16 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const RetiroSchema = new Schema({
+  cliente: {
+    type: String,
+    requiered: true
+  },
   descripcion: {
     type: String,
     required: true,
   },
   estado: {
-    type: String,
+    type: Boolean,
     required: true,
   },
 });
