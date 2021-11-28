3aa2d96d011bb0f3a34d24c12cd64f85a81b1d5f
andylopezgit <alopezciotta@gmail.com>
2021-11-28 11:12:00 -0300

    agrego archivo Proctfile

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ config(app);
 router(app);
 
 const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.listen(port, host, () =>
   console.log(`El servidor ha sido inicializado: http://${process.env.HOST}:${process.env.PORT}`)
