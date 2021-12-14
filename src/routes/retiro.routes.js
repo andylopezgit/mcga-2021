6559809245875ce699225164bb74d7ddecfe5cd5
andylopezgit <alopezciotta@gmail.com>
2021-12-14 09:55:20 -0300

    formato del archivo retiro.routes

--- src/routes/retiro.routes.js
+++ src/routes/retiro.routes.js
@@ -7,22 +7,22 @@ import MensajeController from '../controllers/MensajeController'
 import LoginController from "../controllers/LoginController"
 import RegistroController from '../controllers/Registro.controller'
 
-router.get('/', jwtMiddle , MensajeController.getMensaje)
-router.post('/save-retiro', jwtMiddle , RetiroController.saveRetiro);
+router.get('/', jwtMiddle, MensajeController.getMensaje)
+router.post('/save-retiro', jwtMiddle, RetiroController.saveRetiro);
 router.get('/retiro/:id', RetiroController.getRetiro);
-router.get('/retiros', jwtMiddle , RetiroController.getRetiros);
-router.put('/update-retiro/:id', jwtMiddle , RetiroController.updatedRetiro);
+router.get('/retiros', jwtMiddle, RetiroController.getRetiros);
+router.put('/update-retiro/:id', jwtMiddle, RetiroController.updatedRetiro);
 router.delete('/delete-retiro/:id', RetiroController.deleteRetiro);
 
-router.get('/clientes', jwtMiddle , ClienteController.getClientes);
-router.get('/cliente/:id', jwtMiddle , ClienteController.getCliente);
-router.post('/save-cliente', jwtMiddle , ClienteController.saveCliente);
-router.put('/update-cliente/:id', jwtMiddle , ClienteController.updatedCliente)
-router.delete('/delete-cliente/:id', jwtMiddle , ClienteController.deleteCliente)
+router.get('/clientes', jwtMiddle, ClienteController.getClientes);
+router.get('/cliente/:id', jwtMiddle, ClienteController.getCliente);
+router.post('/save-cliente', jwtMiddle, ClienteController.saveCliente);
+router.put('/update-cliente/:id', jwtMiddle, ClienteController.updatedCliente)
+router.delete('/delete-cliente/:id', jwtMiddle, ClienteController.deleteCliente)
 
 router.post('/login', LoginController.getToken)
 
 router.post('/registro', RegistroController.saveUsuario)
-router.get('/usuarios', jwtMiddle , RegistroController.getUsuarios)
+router.get('/usuarios', jwtMiddle, RegistroController.getUsuarios)
 
 export default router;
