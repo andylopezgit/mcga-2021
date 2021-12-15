9d14f35c5d4d0c5a2bad15f70c9e65f00364cfc3
andylopezgit <alopezciotta@gmail.com>
2021-12-15 20:41:52 -0300

    agrego header a config de cors

--- src/config.js
+++ src/config.js
@@ -8,5 +8,18 @@ export default (app) => {
   app.disable('x-powered-by');  
   app.use(express.json())
   app.use(morgan("dev"));
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+  
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, authorization"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "PUT, GET, POST, DELETE, OPTIONS"
+    );
+    next();
+  });
 
 }
