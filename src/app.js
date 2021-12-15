0dc1c40da4235334311e9927b0c75a369b0e6a2d
andylopezgit <alopezciotta@gmail.com>
2021-12-15 20:53:00 -0300

    mas headers

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 // Config
 config(app);
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin','http://localhost:8080', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
