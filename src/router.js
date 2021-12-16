5e3b5e679459d43000aeed4ad4dff6139526d97a
andylopezgit <alopezciotta@gmail.com>
2021-12-16 11:19:13 -0300

    agrego cors al router

--- src/router.js
+++ src/router.js
@@ -1,8 +1,13 @@
 import retiroRoutes from './routes/retiro.routes'
 import jwtMiddle from './middleware/jwtMiddle'
+import cors from 'cors'
 
 export default (app) => {
 
+  app.use(cors())
+  app.use(() => {
+    console.log('desde router')
+  })
   app.use('/api', /* jwtMiddle */ retiroRoutes);
 
 }
