f022b832cde93613b688f412190344f941a62ace
andylopezgit <alopezciotta@gmail.com>
2021-12-16 13:13:26 -0300

    saco cors options

--- src/router.js
+++ src/router.js
@@ -1,18 +1,11 @@
 import retiroRoutes from './routes/retiro.routes'
-import jwtMiddle from './middleware/jwtMiddle'
-import cors from 'cors'
 
 export default (app) => {
-
-  var corsOptions = {
-    origin: 'http://localhost:8080',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
 
   app.use(cors())
   app.use(() => {
     console.log('desde router')
   })
-  app.use('/api', cors(corsOptions),/* jwtMiddle */ retiroRoutes);
+  app.use('/api',/* jwtMiddle */ retiroRoutes);
 
 }
