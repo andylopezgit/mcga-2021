0b47a0260e987c8989b64123ed5ac1e3741ee9aa
andylopezgit <alopezciotta@gmail.com>
2021-12-16 13:04:03 -0300

    cors

--- src/config.js
+++ src/config.js
@@ -4,10 +4,10 @@ import morgan from 'morgan';
 const express = require("express")
 
 export default (app) => {
+  app.use(cors())
   app.disable('x-powered-by');  
 
   app.use(express.json())
-  app.use(cors());
   app.use(morgan("dev"));
 
 }
