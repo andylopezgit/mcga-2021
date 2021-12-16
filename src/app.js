baa6a98dff3b7cb335f639bb23be912263d1bb30
andylopezgit <alopezciotta@gmail.com>
2021-12-16 10:32:44 -0300

    cambio import cors

--- src/app.js
+++ src/app.js
@@ -3,31 +3,16 @@ import config from "./config";
 import router from './router'
 import "./database"
 import Jwt  from "jsonwebtoken";
+import cors from 'cors'
 const app = express();
-const cors = require('cors');
 
 
 // Config
 config(app);
 app.use(cors())
-app.use((req, res, next) => {
-  console.log('desde middleware')
-  res.header('Access-Control-Allow-Origin','http://localhost:8080', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  console.log('estoy desde los headers')
-  next();
+app.use(() => {
+  console.log('abajo de cors')
 })
-app.use((req, res, next) => {
-  console.log('estoy desde los headers')
-  res.header('Access-Control-Allow-Origin','http://localhost:8080', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  console.log('estoy desde los headers')
-  next();
-});
 
 
 // Router
--- src/config.js
+++ src/config.js
@@ -8,13 +8,6 @@ export default (app) => {
   app.disable('x-powered-by');  
   app.use(express.json())
   app.use(morgan("dev"));
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8080','*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
 
 
 }
