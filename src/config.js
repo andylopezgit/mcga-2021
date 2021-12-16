8ff7d6752321756f66aba65e369c086da1e17c21
andylopezgit <alopezciotta@gmail.com>
2021-12-16 10:18:09 -0300

    cors

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export default (app) => {
   app.use(express.json())
   app.use(morgan("dev"));
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '"http://localhost:8080"','*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080','*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
