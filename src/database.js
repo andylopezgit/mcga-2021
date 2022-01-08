694ee6b81213f74de83b6d837b6a340fd48b76da
andylopezgit <alopezciotta@gmail.com>
2022-01-08 12:38:14 -0300

    reparo error en promise db

--- src/database.js
+++ src/database.js
@@ -6,12 +6,26 @@ const options = {
   useUnifiedTopology: true,
 };
 
-mongoose.Promise = global.Promise;
-mongoose
-  .connect(uri)
-  .then(() => {
-    console.log("Conectado a MongoDB");
-  })
-  .catch(err => {
-    console.error(err, "error con promesa DB");
-  });
+// mongoose.Promise = global.Promise;
+// mongoose
+//   .connect(uri)
+//   .then(() => {
+//     console.log("Conectado a MongoDB");
+//   })
+//   .catch(err => {
+//     console.error(err, "error con promesa DB");
+//   });
+
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  (error) => {
+    if (error) {
+      // eslint-disable-next-line no-console
+      console.log('🔴 Database error: ', error);
+    } else {
+      // eslint-disable-next-line no-console
+      console.log('🟢 Database connected');
+    }
+  },
+);
